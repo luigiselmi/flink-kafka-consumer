8f888823e1809ff851762af196d7ba98e61d633e
Luigi Selmi <luigiselmi@gmail.com>
2016-04-07 20:33:33 +0200

    fixed wrong flink dependency

--- src/main/java/eu/bde/sc4pilot/flink/WindowTrafficData.java
+++ src/main/java/eu/bde/sc4pilot/flink/WindowTrafficData.java
@@ -10,8 +10,6 @@ import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer082;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
