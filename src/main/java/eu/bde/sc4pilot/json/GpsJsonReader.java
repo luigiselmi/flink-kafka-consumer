ba1a6685d05bf137676794ac6c07cc016c3e096c
Luigi Selmi <luigiselmi@gmail.com>
2016-04-12 18:56:31 +0200

    added a method to map input to flink tuples directly

--- src/main/java/eu/bde/sc4pilot/json/GpsJsonReader.java
+++ src/main/java/eu/bde/sc4pilot/json/GpsJsonReader.java
@@ -13,6 +13,14 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class GpsJsonReader {
+  // Position in a Flink Tuple7. Position '0' is reserved for the key
+  public static final int KEY = 0;
+  public static final int RECORDED_TIMESTAMP = 1;
+  public static final int LON = 2;
+  public static final int LAT = 3;
+  public static final int ALTITUDE = 4;
+  public static final int SPEED = 5;
+  public static final int ORIENTATION = 6;
   
   /**
    * Gps data are available as a Json array or records.
@@ -30,11 +38,11 @@ public class GpsJsonReader {
           JsonObject jsonRecord = jsonRecords.get(i).getAsJsonObject();
           gpsRecord.setJsonString(jsonString);
           gpsRecord.setTimestamp(jsonRecord.get("recorded_timestamp").getAsString());
-          gpsRecord.setLon(jsonRecord.get("lon").getAsString());
-          gpsRecord.setLat(jsonRecord.get("lat").getAsString());
-          gpsRecord.setLat(jsonRecord.get("altitude").getAsString());
-          gpsRecord.setLat(jsonRecord.get("speed").getAsString());
-          gpsRecord.setLat(jsonRecord.get("orientation").getAsString());
+          gpsRecord.setLon(jsonRecord.get("lon").getAsDouble());
+          gpsRecord.setLat(jsonRecord.get("lat").getAsDouble());
+          gpsRecord.setAltitude(jsonRecord.get("altitude").getAsDouble());
+          gpsRecord.setSpeed(jsonRecord.get("speed").getAsDouble());
+          gpsRecord.setOrientation(jsonRecord.get("orientation").getAsDouble());
           gpsRecords.add(gpsRecord);
         }
     }
@@ -42,28 +50,27 @@ public class GpsJsonReader {
     return gpsRecords;
   }
   
-  public static Tuple7<String,String,String,String,String,String,String> getGpsTuples(String jsonString) {
-    //ArrayList<GpsRecord> gpsRecords = new ArrayList<GpsRecord>();
-    
+  public static List<Tuple6<String,String,String,String,String,String>> getGpsTuples(String jsonString) {
+    ArrayList<Tuple6<String,String,String,String,String,String>> gpsRecords = new ArrayList<Tuple6<String,String,String,String,String,String>>();
     JsonParser parser = new JsonParser();
     JsonElement element = parser.parse(jsonString);
-    Tuple7<String,String,String,String,String,String,String> tp7 = null;
+    Tuple6<String,String,String,String,String,String> tp6 = null;
     if (element.isJsonArray()) {
         JsonArray jsonRecords = element.getAsJsonArray();        
         for (int i = 0; i < jsonRecords.size(); i++) {
-          tp7 = new Tuple7<String,String, String, String, String, String, String>();
-          JsonObject jsonRecord = jsonRecords.get(i).getAsJsonObject();
-          tp7.setField(jsonString, 0);
-          tp7.setField(jsonRecord.get("recorded_timestamp").getAsString(), 1);
-          tp7.setField(jsonRecord.get("lon").getAsString(), 2);
-          tp7.setField(jsonRecord.get("lat").getAsString(), 3);
-          tp7.setField(jsonRecord.get("altitude").getAsString(), 4);
-          tp7.setField(jsonRecord.get("speed").getAsString(), 5);
-          tp7.setField(jsonRecord.get("orientation").getAsString(), 6);
+          tp6 = new Tuple6<String, String, String, String, String, String>();
+          JsonObject jsonRecord = jsonRecords.get(i).getAsJsonObject();          
+          tp6.setField(jsonRecord.get("recorded_timestamp").getAsString(), RECORDED_TIMESTAMP);
+          tp6.setField(jsonRecord.get("lon").getAsString(), LON);
+          tp6.setField(jsonRecord.get("lat").getAsString(), LAT);
+          tp6.setField(jsonRecord.get("altitude").getAsString(), ALTITUDE);
+          tp6.setField(jsonRecord.get("speed").getAsString(), SPEED);
+          tp6.setField(jsonRecord.get("orientation").getAsString(), ORIENTATION);
+          gpsRecords.add(tp6);
         }
     }
    
-    return tp7;
+    return gpsRecords;
   }
   
   
