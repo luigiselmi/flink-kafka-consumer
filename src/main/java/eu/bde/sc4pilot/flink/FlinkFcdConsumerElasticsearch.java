5ffa5819eb50495e92d611d65c9643a298d74b81
Luigi Selmi <luigiselmi@gmail.com>
2018-02-13 02:17:07 +0100

    added sink Elasticsearch

--- src/main/java/eu/bde/sc4pilot/flink/FlinkFcdConsumerElasticsearch.java
+++ src/main/java/eu/bde/sc4pilot/flink/FlinkFcdConsumerElasticsearch.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -16,6 +17,7 @@ import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.tuple.Tuple6;
 import org.apache.flink.api.java.tuple.Tuple9;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -32,6 +34,10 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.util.Collector;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Requests;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.google.common.io.Resources;
@@ -111,11 +117,11 @@ public class FlinkFcdConsumerElasticsearch {
 	  
 	// map-match locations given as (longitude, latitude) pairs to  streets
 	  //DataStream<Tuple9<Integer,String,Double,Double,Double,Integer,Double,Integer,String>> streamMatchedTuples = events
-	  DataStream<Tuple4<String,Double,String,Integer>> streamMatchedTuples = events
+	  DataStream<Tuple6<String,Double,Double, Double, Integer, String>> streamMatchedTuples = events
 			  .flatMap(new MapMatcher())
-			  .keyBy(8)
-			  .timeWindow(Time.seconds(TIME_WINDOW_PARAM_VALUE))
-			  .apply(new AverageSpeed());
+			  .keyBy(8) // key by road segment identifier
+			  .timeWindow(Time.minutes(TIME_WINDOW_PARAM_VALUE))
+			  .apply(new RoadSegment());
 			  
 	
 	  // Counts the events that happen in any cell within the bounding box
@@ -130,9 +136,9 @@ public class FlinkFcdConsumerElasticsearch {
 			.apply(new EventCounter());
 	*/
 	  // stores the data in Elasticsearch
-	 // saveFcdDataElasticsearch(boxBoundedEvents);
+	  saveFcdDataElasticsearch(streamMatchedTuples);
 	  
-	  streamMatchedTuples.print();
+	  //streamMatchedTuples.print();
     
     env.execute("Read Taxi Data from Kafka");
   
@@ -220,9 +226,9 @@ public class FlinkFcdConsumerElasticsearch {
    * 4) the number of records used, that is the number of times a taxi sent a gps message from that same road segment
    *
    */
-  public static class AverageSpeed implements WindowFunction<
+  public static class RoadSegment implements WindowFunction<
                   Tuple9<Integer,String,Double,Double,Double,Integer,Double,Integer,String>,
-                  Tuple4<String,Double,String,Integer>,
+                  Tuple6<String, Double, Double, Double, Integer, String>,
                   Tuple,
                   TimeWindow> {
 
@@ -231,18 +237,34 @@ public class FlinkFcdConsumerElasticsearch {
         Tuple key,
         TimeWindow window,
         Iterable<Tuple9<Integer,String,Double,Double,Double,Integer,Double,Integer,String>> records,
-        Collector<Tuple4<String, Double, String, Integer>> out) throws Exception {
-      int count = 0;
+        Collector<Tuple6<String, Double, Double, Double, Integer, String>> out) throws Exception {
+      int count = 0; // number of vehicles in the road segment within the time window
       String roadSegmentId = "N/A"; // OpenStreetMap identifier of a road segment (between two junctions)
-      double speedAccumulator = 0.0;
+      double averageSpeed = 0.0;
+      double meanLatitude = 0.0;
+      double meanLongitude = 0.0;
+      
       for (Tuple9<Integer,String,Double,Double,Double,Integer,Double,Integer,String> tuple9: records){
         int speed = tuple9.getField(GpsJsonReader.SPEED);
+        double longitude = tuple9.getField(2);
+        double latitude = tuple9.getField(3);
+        meanLatitude += latitude;
+        meanLongitude += longitude;
         count++;
-        speedAccumulator += speed;
+        averageSpeed += speed;
         roadSegmentId = tuple9.getField(GpsJsonReader.OSM_LINK);
       }
-      double averageSpeed = speedAccumulator / count; 
-      out.collect(new Tuple4<>(KAFKA_TOPIC_PARAM_VALUE,averageSpeed, roadSegmentId,count));
+      
+      averageSpeed = averageSpeed / count; 
+      meanLongitude = meanLongitude / count;
+      meanLatitude = meanLatitude / count;
+      
+      //DateTime athensTime = new DateTime(window.getEnd(), DateTimeZone.forID("Europe/Athens"));
+      DateTimeFormatter timeFormatter =DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+      //String timestamp = athensTime.toString(timeFormatter);
+      String timestamp = timeFormatter.print(window.getEnd());
+      out.collect(new Tuple6<>(roadSegmentId,meanLongitude, meanLatitude, averageSpeed, count, timestamp));
+      //System.out.println(roadSegmentId + ", " + meanLongitude + ", " + meanLatitude + ", " + averageSpeed + ", " + count + ", " + timestamp);
     }
     
   }
@@ -269,41 +291,43 @@ public class FlinkFcdConsumerElasticsearch {
    * @throws UnknownHostException
    */
 	public static void saveFcdDataElasticsearch(
-			DataStream<Tuple5<Integer, Double, Double, Integer, String>> inputStream) throws UnknownHostException {
+		DataStream<Tuple6<String, Double, Double, Double, Integer, String>> inputStream) throws UnknownHostException {
 		Map<String, String> config = new HashMap<>();
 		// This instructs the sink to emit after every element, otherwise they would be
 		// buffered
 		config.put("bulk.flush.max.actions", "1");
-		config.put("cluster.name", "elasticsearch");
+		config.put("cluster.name", "pilot-sc4");
 
 		List<InetSocketAddress> transports = new ArrayList<InetSocketAddress>();
 		log.info("XXXXX (InetAddress.getByName(elasticsearch), 9300))");
-		transports.add(new InetSocketAddress(InetAddress.getByName("elasticsearch"), 9300));
-		// transports.add(new InetSocketTransportAddress("172.19.0.2", 9300)); //
-		// remember experiment to address elasticsearch in s swarm -- not successful
+		transports.add(new InetSocketAddress(InetAddress.getByName("localhost"), 9300));
 
-		inputStream.addSink(new ElasticsearchSink<Tuple5<Integer, Double, Double, Integer, String>>(config, transports,
-				new ElasticsearchSinkFunction<Tuple5<Integer, Double, Double, Integer, String>>() {
-
-					public IndexRequest createIndexRequest(Tuple5<Integer, Double, Double, Integer, String> record) {
-						Map<String, Object> json = new HashMap<>();
-						json.put("cellid", record.f0);
-						json.put("location", record.f1.toString() + "," + record.f2.toString()); // lat,lon
-						json.put("vehicles", record.f3);
-						json.put("timestamp", record.f4);
-
-						return Requests.indexRequest().index("thessaloniki").type("floating-cars").source(json);
-
-					}
-
-          @Override
-          public void process(Tuple5<Integer, Double, Double, Integer, String> arg0, RuntimeContext arg1,
-              RequestIndexer arg2) {
-            // TODO Auto-generated method stub
-            
-          }
-					
-				}));
+		inputStream.addSink(new ElasticsearchSink<>(config, transports, new RoadSegmentInserter()));
+    
 	}
+	
+	public static class RoadSegmentInserter implements ElasticsearchSinkFunction<Tuple6<String, Double, Double, Double, Integer, String>> {
+    
+    @Override
+    public void process(Tuple6<String, Double, Double, Double, Integer, String> record, RuntimeContext ctx,
+        RequestIndexer indexer) {
+       
+        Map<String, Object> json = new HashMap<>();
+        json.put("roadsegmentid", record.f0);
+        json.put("location", record.f2.toString() + "," + record.f1.toString()); // lat,lon
+        json.put("speed", record.f3);
+        json.put("count", record.f4);
+        json.put("timestamp", record.f5);
+
+        IndexRequest rqst = Requests.indexRequest().index("thessaloniki").type("floating-cars").source(json);
+      
+        indexer.add(rqst);
+
+    }
+
+      
+}
+
+
 
 }
